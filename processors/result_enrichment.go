6bd4ff91002fdb7461e6f16f7fa620d51941afc2
cx-rotems <rotem.sheps@checkmarx.com>
2025-01-15 23:00:41 +0200

    change log

--- processors/result_enrichment.go
+++ processors/result_enrichment.go
@@ -23,7 +23,7 @@ func (re *ResultEnrichment) Start() {
 	for job := range re.enrichmentChan {
 		for i := 0; i < len(job.Result); i++ {
 			time.Sleep(60 * time.Millisecond) // simulate result enrichment
-			fmt.Printf("ResultEnrichment: Enriching result for job ID %d\n", job.ID)
+			fmt.Printf("ResultEnrichment: Enriching result for result ID %d and job ID  %d\n",  job.Result[i].ResultID,  job.Result[i].JobID) // simulate result enrichment
 		}
 		re.loaderChan <- job
 	}
